b2f28596627cfd9d131e980eef1ccac3b08d175d
Maximilien L. Bullett <31956266+mlbullett@users.noreply.github.com>
2023-04-12 21:45:10 +0200

    More changes

--- configs/waybar/modules/weather.sh
+++ configs/waybar/modules/weather.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-API_KEY=$(cat ~/.local/bin/private/.darksky_api_key)
+API_KEY=$(cat ~/.local/bin/private/.openweather_key)
 CACHE=~/.cache/weatherapi.json
 
 if [ ! -f $CACHE ]; then
@@ -7,13 +7,15 @@ if [ ! -f $CACHE ]; then
 fi
 
 LOC=$(curl -s ipinfo.io | jq -r .loc)
+LAT=$(echo $LOC | cut -f 1 -d ",")
+LON=$(echo $LOC | cut -f 2 -d ",")
 
 CACHEAGE=$(($(date +%s) - $(stat -c '%Y' "$CACHE")))
 if [ $CACHEAGE -gt 1740 ] || [ ! -s $CACHE ]; then
-    curl -s "https://api.darksky.net/forecast/$API_KEY/$LOC?units=si" -o $CACHE
+    curl -s "https://api.openweathermap.org/data/2.5/weather?lat=$LAT&lon=$LON&appid=$API_KEY&units=metric" -o $CACHE
 fi
 
-TEMP=$(printf "%.0f" "$(jq -r .currently.temperature $CACHE)")
-SUMMARY=$(jq -r .hourly.summary $CACHE)
+TEMP=$(printf "%.0f" "$(jq -r .main.feels_like $CACHE)")
+SUMMARY=$(jq -r .weather[0].description $CACHE)
 
 echo -e "{\"text\":\"| $TEMP°C $SUMMARY\"}"
